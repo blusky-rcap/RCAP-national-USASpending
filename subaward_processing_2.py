3650b55315cd211afa0ee4901c2e9cfc1b223177
blusky-rcap <99436230+blusky-rcap@users.noreply.github.com>
2024-04-23 13:08:05 -0700

    Update subaward_processing_2.py

--- subaward_processing_2.py
+++ subaward_processing_2.py
@@ -9,7 +9,7 @@ def confirm_full_state_name(x):
     except KeyError:
         print('hey its BAD ', x)
 
-
+# this is for checking against water related keywords in award descriptions
 def keyword_check(row):
     desc = row['subaward_description']
     print(type(row['prime_cfda']), ' ', row['prime_cfda'])
@@ -27,31 +27,20 @@ def keyword_check(row):
     return False
 
 
-
+# read in data outputted by prev file
 nat_df = dd.read_csv('subs_filtered_CFDA_3.csv', converters=dtypes_in, engine='python', on_bad_lines='warn')
-
+# QA/QC measures discovered to be necessary
 nat_df['subawardee_city_name'] = nat_df['subawardee_city_name'].str.replace(r'"', '')
 nat_df['subawardee_city_name'] = nat_df['subawardee_city_name'].str.split(',').str[0]
 nat_df['subawardee_city_name'] = nat_df['subawardee_city_name'].str.replace(r'.', '')
-
-
 nat_df['subaward_primary_place_of_performance_state_name'] = nat_df['subaward_primary_place_of_performance_state_name'].apply(lambda x: best_match(x), meta=('subaward_primary_place_of_performance_state_name', str))
-
-print('checkinggggg ============================================')
-
-#df[df['menu_item'].str.contains('fresh|spaghetti')]
-#water|pipe|drain|wastewater|sewer|sewage|drinking|sink|pump|storm|filtration|faucet|fountain|sump|lead|stormwater|water main|treatment plant|water wells|hydrant|plumping|sanitary
-#df4[df4['col'].str.contains('|'.join(terms))]
-#k_df = nat_df[nat_df['subaward_description'].str.contains('|'.join(water_keywords), na=False)]
 nat_df['subaward_description'] = nat_df.subaward_description.astype(str)
-
+# search for water related keywords
 m = nat_df.apply(keyword_check, axis=1)
 new_df = nat_df[m]
-#print(new_df)
-#new_df.to_csv('applyTest2.csv', single_file=True)
-#keyword_df = nat_df[nat_df['subaward_description'].str.contains('water', na=False)]
 new_df.to_csv('keyword_filtered_subs4.csv', single_file=True)
 print('sorted by keywords')
+# new grouping op
 groupby_cols = [
                 'subawardee_city_name',
                 #'recipient_county_name',
@@ -59,10 +48,8 @@ groupby_cols = [
                 'subaward_primary_place_of_performance_state_name',
                 'prime_cfda'
                ]
-
-print('now gonna group...')
+print('now gonna group...')  # print statements for debugging
 grpd = new_df.groupby(groupby_cols)
 print('group now saving')
 grpd.sum().reset_index().to_csv('sub7.csv', single_file=True)
-
 print('saved')
