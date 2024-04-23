13cf551001fce03692782b781c5d3de94a91a458
blusky-rcap <99436230+blusky-rcap@users.noreply.github.com>
2024-04-23 13:08:52 -0700

    Update subaward_processing_3.py

--- subaward_processing_3.py
+++ subaward_processing_3.py
@@ -4,7 +4,6 @@ from efc_functions import *
 
 nat_df = pd.read_csv('sub7.csv')
 nat_dropped = nat_df.drop(columns=['prime_award_amount'])
-#df.rename(columns={"A": "a", "B": "c"})
 new_col_names = {
     'subawardee_city_name': 'recipient_city_name',
     'subaward_primary_place_of_performance_state_name': 'primary_place_of_performance_state_name',
@@ -12,7 +11,7 @@ new_col_names = {
     'subaward_amount': 'total_obligated_amount'
 }
 nat_dropped.rename(columns=new_col_names, inplace=True)
-
+# pivot the table so that CFDA nums are column names
 p1 = pd.pivot_table(nat_dropped, values=['total_obligated_amount'],
                     index=['recipient_city_name',
                            #'recipient_county_name',
@@ -24,6 +23,6 @@ p1 = pd.pivot_table(nat_dropped, values=['total_obligated_amount'],
                     margins=True
                     ).reset_index()
 
-
+# save pivoted table
 p1.to_csv('o_sub9_pivoted.csv')
 print("saved")
